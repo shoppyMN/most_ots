fcc7ab17cfb29320d423adbe2bb5d75fa88fabde
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2018-01-28 13:38:30 +0000

    Small Refactor

--- lib/most_ots/service.rb
+++ lib/most_ots/service.rb
@@ -1,4 +1,3 @@
-require 'net/http'
 require 'json'
 require 'openssl'
 require 'base64'
@@ -11,81 +10,94 @@ module MostOts
     include Logging
     # Fields that needs to be encrypted
     ENCRYPTED_FIELDS = %i[traceNo qrAccountNumber qrCode srcMsisdn tan].freeze
-
+    PROTOCOL_VERSION = '05'.freeze
+    
+    # SSL cipher key and IV
     attr_accessor :cipher_key, :cipher_iv
 
+    # Base Parameters
+    # @return [Hash]
+    attr_accessor :base_params
+    
+    # Most Provided Public Key
+    # @return [File] or [String] certificate_string
+    attr_accessor :certificate
+    
+    # Most API Endpoint
+    # @return [String] MOST Service HOST
+    attr_accessor :api_host
+    
     # @param [Hash] options
     def initialize(options = {})
       logger.debug('MostOts Service API Initializing')
-      @base_params = {
+      self.base_params = {
         lang: options.fetch(:lang) { '0' },
-        srcInstId: options.fetch(:src_inst_id) { ENV['SRC_INST_ID'] }
+        srcInstId: options.fetch(:src_inst_id) { ENV['SRC_INST_ID'] },
+        channel: options[:channel],
+        deviceMac: options[:device_mac],
+        deviceIp: options[:device_ip],
+        deviceName: options[:device_name],
+        payeeId: options[:payee_id],
+        qrBankCode: options[:qr_bank_code],
+        qrAccountName: options[:qr_account_name],
+        qrAccountNumber: options[:qr_account_number],
+        tranCur: options[:tran_cur],
+        posNo: options[:pos_no]
       }
-      @protocol_version = options.fetch(:protocol_version) { '05' }
-      @most_cert_file = options.fetch(:most_cert_file)
-      @api_host = options.fetch(:host) { 'http://202.131.242.165:9089' }
-
-      @payee_id = options[:payee_id] if options.key?(:payee_id)
-      @qr_bank_code = options[:qr_bank_code] if options.key?(:qr_bank_code)
-      @qr_account_name = options[:qr_account_name] if options.key?(:qr_account_name)
-      @qr_account_number = options[:qr_account_number] if options.key?(:qr_account_number)
-      @tran_cur = options[:tran_cur] if options.key?(:tran_cur)
-      @pos_no = options[:pos_no] if options.key?(:pos_no)
-      @base_params[:channel] = options[:channel] if options.key?(:channel)
-      @base_params[:device_mac] = options[:device_mac] if options.key?(:device_mac)
-      @base_params[:device_ip] = options[:device_ip] if options.key?(:device_ip)
-      @base_params[:device_name] = options[:device_name] if options.key?(:device_name)
-      @cipher_key = options[:cipher_key]
-      @cipher_iv = options.fetch(:cipher_iv)
-
-      @user_agent = "most_ots/#{VERSION} ruby/#{RUBY_VERSION}"
-      @user_agent << " #{options[:app_name]}/#{options[:app_version]}" if options.key?(:app_name) && options.key?(:app_version)
+      self.certificate = options.fetch(:most_cert_file)
+      self.api_host = options.fetch(:host) { 'http://202.131.242.165:9089' }
+      self.cipher_key = options[:cipher_key]
+      self.cipher_iv = options.fetch(:cipher_iv)
     end
 
     def purchase_qr(params)
       logger.debug('MostOts Service Purchase QR Called')
-      base = {}
-      base[:payeeId] = @payee_id unless @payee_id.nil?
-      base[:posNo] = @pos_no unless @pos_no.nil?
-      base[:tranCur] = @tran_cur unless @tran_cur.nil?
-      api_request('/api/mapi/TT3051', base.merge(params))
+      # Mandatory Fields
+      mf = %i[srcInstId channel lang traceNo payeeId posNo tranAmount tranCur tranDesc]
+      # Optional Fields 
+      of = %i[billId deviceIP deviceMac deviceName]
+      api_request('/api/mapi/TT3051', configure_params(params, mf, of))
     end
 
     def transfer_qr(params)
       logger.debug('MostOts Service Transfer QR Called')
-      base = {}
-      base[:qrBankCode] = @qr_bank_code unless @qr_bank_code.nil?
-      base[:qrAccountName] = @qr_account_name unless @qr_account_name.nil?
-      base[:qrAccountNumber] = @qr_account_number unless @qr_account_number.nil?
-      base[:tranCur] = @tran_cur unless @tran_cur.nil?
-      api_request('/api/mapi/TT3061', params)
+      # Mandatory Fields
+      mf = %i[srcInstId channel lang traceNo qrBankCode qrAccountName qrAccountNumber tranAmount tranCur]
+      # Optional Fields
+      of = %i[tranDesc]
+      api_request('/api/mapi/TT3061', configure_params(params, mf, of))
     end
 
     def wait_payment_response(params)
       logger.debug('MostOts Service Waiting Payment Response')
-      base = {}
-      base[:payeeId] = @payee_id unless @payee_id.nil?
-      base[:posNo] = @pos_no unless @pos_no.nil?
-      api_request('/api/mapi/TT3064', params)
+      # Mandatory Fields
+      mf = %i[srcInstId channel lang qrBankCode qrAccountName qrAccountNumber tranAmount tranCur]
+      # Optional Fields
+      of = %i[tranDesc]
+      api_request('/api/mapi/TT3064', configure_params(params, mf, of))
     end
 
     def check_qr_payment(params)
       logger.debug('MostOts Service Check QR Payment Called')
-      base = {}
-      base[:payeeId] = @payee_id unless @payee_id.nil?
-      base[:posNo] = @pos_no unless @pos_no.nil?
-      base[:tranCur] = @tran_cur unless @tran_cur.nil?
-      api_request('/api/mapi/TT3065', params)
+      # Mandatory Fields
+      mf = %i[srcInstId channel lang traceNo qrCode payeeId posNo billId isCheckQr]
+      # Optional Fields
+      of = %i[deviceIP deviceMac deviceName]
+      api_request('/api/mapi/TT3065', configure_params(params, mf, of))
     end
 
     # @param [Object] params
     def purchase_tan(params)
       logger.debug('MostOts Service Purchase Tan Called')
-      base = {}
-      base[:payeeId] = @payee_id unless @payee_id.nil?
-      base[:posNo] = @pos_no unless @pos_no.nil?
-      base[:tranCur] = @tran_cur unless @tran_cur.nil?
-      api_request('/api/mapi/TT1608', params)
+      # Mandatory Fields
+      mf = %i[srcInstId channel lang traceNo payeeId posNo srcMsisdn tan tranAmount tranCur]
+      # Optional Fields
+      of = %i[billId tranDesc deviceIP deviceMac deviceName]
+      api_request('/api/mapi/TT1608', configure_params(params, mf, of))
+    end
+
+    def check_network
+      logger.debug('MostOts Checking Network Connection')
     end
 
     private
@@ -98,8 +110,8 @@ module MostOts
       cipher = OpenSSL::Cipher.new('AES-128-CBC')
       cipher.encrypt
       cipher.iv = @cipher_iv.pack('c*')
-      rsa = OpenSSL::PKey::RSA.new @most_cert_file
-      key = @cipher_key.nil? ? cipher.random_key : @cipher_key
+      rsa = OpenSSL::PKey::RSA.new self.certificate
+      key = self.cipher_key.nil? ? cipher.random_key : self.cipher_key
       cipher.key = key
       {
         SD: Base64.encode64("#{cipher.update(data)}#{cipher.final}"),
@@ -112,34 +124,46 @@ module MostOts
     # @param [Hash] params
     # @return [Object] response
     def api_request(path, params = nil)
-      params = generate_params(params)
+      params = encrypt_params(params)
       headers = {
         'Content-Type' => 'application/json',
         :Accept => 'application/json',
         'Accept-Charset' => 'utf-8',
         :TT => path.split('TT').last,
         :RS => '00',
-        'User-Agent' => @user_agent,
-        :PV => @protocol_version
+        'User-Agent' => user_agent,
+        :PV => PROTOCOL_VERSION
       }
-      logger.debug("Service Called With endpoint #{@api_host}#{path} params: #{params}")
-      logger.debug("headers: #{headers}")
-      req = HTTParty.post(
-        "#{@api_host}#{path}",
-        body: params,
-        headers: headers
-      )
+      logger.debug("Service Called With endpoint #{self.api_host}#{path} params: #{params}")
+      req = HTTParty.post("#{self.api_host}#{path}", body: params, headers: headers)
       JSON.parse(req.body)
     end
 
+    def configure_params(params, mandatory_fields, optional_fields = [])
+      params = self.base_params.merge(params)
+      processed_params = {}
+      required = mandatory_fields.select{|mk| params[mk].nil? }
+      if required.any?
+        raise "Following mandatory Fields cannot be nil: #{required.join('.')}"
+      end
+      params.each do |k, v|
+        processed_params[k] = v if (mandatory_fields + optional_fields).include?(k) && !v.nil?
+      end
+      processed_params
+    end
+
     # @param [Hash] params
-    def generate_params(params)
+    # @return [String] json_string
+    def encrypt_params(params)
       to_encrypt = {}
       ENCRYPTED_FIELDS.each do |encrypted_key|
         to_encrypt[encrypted_key] = params.delete(encrypted_key) if params.key?(encrypted_key)
       end
-      JSON.generate(@base_params.merge(params).merge(encrypt(to_encrypt)))
+      JSON.generate(params.merge(encrypt(to_encrypt)))
     end
 
+    def user_agent
+      "most_ots/#{VERSION} ruby/#{RUBY_VERSION}"
+    end
   end
 end
